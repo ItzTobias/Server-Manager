ed44c90c72b32ad88154da67ef277ef02cd7c299
Tobias [PC] <itztobiias@gmail.com>
2021-02-12 20:22:49 +0100

    Added new Colors
    - Added Color "Line" (#373737; 55, 55, 55)
    - Added Color "ScrollBarThumbHover" (#636363; 99, 99, 99)
    - Replaced some Color codes in Menu.xaml and ServerInfo.xaml with Staticresources

--- App.xaml.cs
+++ App.xaml.cs
@@ -26,46 +26,50 @@ namespace Server_Manager
 
     public static class Colors
     {
-        public static readonly Color red         = new Color() { R = 212, G = 68 , B = 16 , A = 255 };
-        public static readonly Color redHover    = new Color() { R = 231, G = 100, B = 58 , A = 255 };
+        public static readonly Color red                 = new Color() { R = 212, G = 68 , B = 16 , A = 255 };
+        public static readonly Color redHover            = new Color() { R = 231, G = 100, B = 58 , A = 255 };
 
-        public static readonly Color green       = new Color() { R = 0  , G = 133, B = 64 , A = 255 };
-        public static readonly Color greenHover  = new Color() { R = 13 , G = 209, B = 100, A = 255 };
+        public static readonly Color green               = new Color() { R = 0  , G = 133, B = 64 , A = 255 };
+        public static readonly Color greenHover          = new Color() { R = 13 , G = 209, B = 100, A = 255 };
 
-        public static readonly Color input       = new Color() { R = 18 , G = 18 , B = 18 , A = 255 };
-        public static readonly Color darkGray    = new Color() { R = 30 , G = 30 , B = 30 , A = 255 };
-        public static readonly Color gray        = new Color() { R = 40 , G = 40 , B = 40 , A = 255 };
-        public static readonly Color lightGray   = new Color() { R = 48 , G = 48 , B = 48 , A = 255 };
-        public static readonly Color fontColor   = new Color() { R = 204, G = 204, B = 204, A = 255 };
-        public static readonly Color grayHover   = new Color() { R = 69 , G = 69 , B = 69 , A = 255 };
+        public static readonly Color input               = new Color() { R = 18 , G = 18 , B = 18 , A = 255 };
+        public static readonly Color darkGray            = new Color() { R = 30 , G = 30 , B = 30 , A = 255 };
+        public static readonly Color gray                = new Color() { R = 40 , G = 40 , B = 40 , A = 255 };
+        public static readonly Color lightGray           = new Color() { R = 48 , G = 48 , B = 48 , A = 255 };
+        public static readonly Color fontColor           = new Color() { R = 204, G = 204, B = 204, A = 255 };
+        public static readonly Color grayHover           = new Color() { R = 69 , G = 69 , B = 69 , A = 255 };
+        public static readonly Color line                = new Color() { R = 55 , G = 55 , B = 55 , A = 255 };
+        public static readonly Color scrollBarThumbHover = new Color() { R = 99 , G = 99 , B = 99 , A = 255 };
 
-        public static readonly Color brightness1 = new Color() { R = 78 , G = 78 , B = 78 , A = 255 };
-        public static readonly Color brightness2 = new Color() { R = 89 , G = 89 , B = 89 , A = 255 };
-        public static readonly Color brightness3 = new Color() { R = 111, G = 111, B = 111, A = 255 };
-        public static readonly Color brightness4 = new Color() { R = 114, G = 114, B = 114, A = 255 };
-        public static readonly Color brightness5 = new Color() { R = 138, G = 138, B = 138, A = 255 };
-        public static readonly Color brightness6 = new Color() { R = 209, G = 209, B = 209, A = 255 };
+        public static readonly Color brightness1         = new Color() { R = 78 , G = 78 , B = 78 , A = 255 };
+        public static readonly Color brightness2         = new Color() { R = 89 , G = 89 , B = 89 , A = 255 };
+        public static readonly Color brightness3         = new Color() { R = 111, G = 111, B = 111, A = 255 };
+        public static readonly Color brightness4         = new Color() { R = 114, G = 114, B = 114, A = 255 };
+        public static readonly Color brightness5         = new Color() { R = 138, G = 138, B = 138, A = 255 };
+        public static readonly Color brightness6         = new Color() { R = 209, G = 209, B = 209, A = 255 };
 
 
-        public static readonly SolidColorBrush redBrush         = new SolidColorBrush(red        );
-        public static readonly SolidColorBrush redHoverBrush    = new SolidColorBrush(redHover   );
+        public static readonly SolidColorBrush redBrush                 = new SolidColorBrush(red                );
+        public static readonly SolidColorBrush redHoverBrush            = new SolidColorBrush(redHover           );
 
-        public static readonly SolidColorBrush greenBrush       = new SolidColorBrush(green      );
-        public static readonly SolidColorBrush greenHoverBrush  = new SolidColorBrush(greenHover );
+        public static readonly SolidColorBrush greenBrush               = new SolidColorBrush(green              );
+        public static readonly SolidColorBrush greenHoverBrush          = new SolidColorBrush(greenHover         );
 
-        public static readonly SolidColorBrush inputBrush       = new SolidColorBrush(input      );
-        public static readonly SolidColorBrush darkGrayBrush    = new SolidColorBrush(darkGray   );
-        public static readonly SolidColorBrush grayBrush        = new SolidColorBrush(gray       );
-        public static readonly SolidColorBrush lightGrayBrush   = new SolidColorBrush(lightGray  );
-        public static readonly SolidColorBrush fontColorBrush   = new SolidColorBrush(fontColor  );
-        public static readonly SolidColorBrush grayHoverBrush   = new SolidColorBrush(grayHover  );
+        public static readonly SolidColorBrush inputBrush               = new SolidColorBrush(input              );
+        public static readonly SolidColorBrush darkGrayBrush            = new SolidColorBrush(darkGray           );
+        public static readonly SolidColorBrush grayBrush                = new SolidColorBrush(gray               );
+        public static readonly SolidColorBrush lightGrayBrush           = new SolidColorBrush(lightGray          );
+        public static readonly SolidColorBrush fontColorBrush           = new SolidColorBrush(fontColor          );
+        public static readonly SolidColorBrush grayHoverBrush           = new SolidColorBrush(grayHover          );
+        public static readonly SolidColorBrush lineBrush                = new SolidColorBrush(line               );
+        public static readonly SolidColorBrush scrollBarThumbHoverBrush = new SolidColorBrush(scrollBarThumbHover);
 
-        public static readonly SolidColorBrush brightness1Brush = new SolidColorBrush(brightness1);
-        public static readonly SolidColorBrush brightness2Brush = new SolidColorBrush(brightness2);
-        public static readonly SolidColorBrush brightness3Brush = new SolidColorBrush(brightness3);
-        public static readonly SolidColorBrush brightness4Brush = new SolidColorBrush(brightness4);
-        public static readonly SolidColorBrush brightness5Brush = new SolidColorBrush(brightness5);
-        public static readonly SolidColorBrush brightness6Brush = new SolidColorBrush(brightness6);
+        public static readonly SolidColorBrush brightness1Brush         = new SolidColorBrush(brightness1        );
+        public static readonly SolidColorBrush brightness2Brush         = new SolidColorBrush(brightness2        );
+        public static readonly SolidColorBrush brightness3Brush         = new SolidColorBrush(brightness3        );
+        public static readonly SolidColorBrush brightness4Brush         = new SolidColorBrush(brightness4        );
+        public static readonly SolidColorBrush brightness5Brush         = new SolidColorBrush(brightness5        );
+        public static readonly SolidColorBrush brightness6Brush         = new SolidColorBrush(brightness6        );
 
     }
 
